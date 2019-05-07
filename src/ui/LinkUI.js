d011ec29a0166bc667f4ee568386d253c03fdc7b
Emmanuel Pire <piremmanuel@gmail.com>
2019-05-07 13:09:04 +0200

    Improve link rendering

--- src/ui/LinkUI.js
+++ src/ui/LinkUI.js
@@ -9,7 +9,7 @@ export default class LinkUI extends EventEmitter{
     this.layer = opts.layer
     this.toInlet = opts.toInlet
     this.fromOutlet = opts.fromOutlet
-    this.bendy = opts.bendy
+    this.bendy = opts.bendy 
 
     this.line = new Konva.Line({
       stroke: opts.color,
@@ -36,7 +36,36 @@ export default class LinkUI extends EventEmitter{
     const points = [from.x, from.y]
     if(this.bendy){
       points.push((from.x+to.x)/2, ((from.y+to.y)/2) + 18)
+    } else {
+      const fromOutlet = this.from.getOutlet(this.fromOutlet)
+      const toInlet = this.to.getInlet(this.toInlet)
+
+      if(fromOutlet.side==='right'){
+        points.push(from.x+10, from.y)
+      }
+
+      if(fromOutlet.side==='bottom'){
+        points.push(from.x, from.y+10)
+      }
+      
+      if(fromOutlet.side === 'right' && toInlet.side === 'left' && from.x>to.x){
+        if(from.y < to.y){
+          points.push(from.x+10, from.y+30)
+          points.push(to.x-5, to.y-30)
+        } else {
+          points.push(from.x+10, from.y-30)
+          points.push(to.x-5, to.y+30)
+        }
+      }
+
+      
+      if(toInlet.side==='left'){
+        points.push(to.x-5, to.y)
+      }
+      
     }
+
+
     points.push(to.x, to.y)
     this.line.points(points)
 
