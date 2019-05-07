b0a9de6da0852147a2b5cf4fe9cb79d82a29cd46
Emmanuel Pire <piremmanuel@gmail.com>
2019-05-07 14:04:12 +0200

    Make links larger on hover

--- src/ui/LinkUI.js
+++ src/ui/LinkUI.js
@@ -15,6 +15,14 @@ export default class LinkUI extends EventEmitter{
       stroke: opts.color,
       tension: this.bendy ? 0.6 : 0
     })
+    this.line.on('mouseenter', () => {
+      this.line.strokeWidth(5)
+      this.line.draw()
+    })
+    this.line.on('mouseleave', () => {
+      this.line.strokeWidth(2)
+      this.layer.draw()
+    })
     this.reposition()
 
     this.line.on('dblclick', () => {
