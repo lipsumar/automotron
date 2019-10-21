e7fa586a51f848c8351e610bcd74ecf43e5caa99
Emmanuel Pire <piremmanuel@gmail.com>
2019-10-21 21:53:54 +0200

    finished agreement lib

--- src/automotron/agreementUtils.js
+++ src/automotron/agreementUtils.js
@@ -9,7 +9,7 @@ function parse(str) {
   const { rawValues, rawFlags } = _parseParts(str);
   const agreement = _getAgreement(rawFlags)
   const values = _getValues(rawValues, agreement)
-  return {agreement}
+  return { agreement, values }
 }
 
 /**
@@ -35,32 +35,32 @@ function _parseParts(str) {
  * Ex: 'ms' => {m:true, f:false, s:true, p:false}
  * @param {str} rawFlags 
  */
-function _getAgreement(rawFlags){
+function _getAgreement(rawFlags) {
   const flags = rawFlags.split('')
-  const agreement = {m:false, f:false, s:false, p:false}
+  const agreement = { m: false, f: false, s: false, p: false }
 
   flags.forEach(flag => {
-    if(flag === '*') return
+    if (flag === '*') return
     agreement[flag] = true
   })
 
-  if(flags.length === 1){
-    if(flags[0] === 's' || flags[0] === 'p'){
+  if (flags.length === 1) {
+    if (flags[0] === 's' || flags[0] === 'p') {
       agreement.m = true
       agreement.f = true
     }
-    if(flags[0] === 'm' || flags[0] === 'f'){
+    if (flags[0] === 'm' || flags[0] === 'f') {
       agreement.s = true
       agreement.p = true
     }
   }
 
-  if(flags[0] === '*'){
+  if (flags[0] === '*') {
     agreement.m = true
     agreement.f = true
   }
 
-  if(flags[1] === '*'){
+  if (flags[1] === '*') {
     agreement.s = true
     agreement.p = true
   }
@@ -68,10 +68,49 @@ function _getAgreement(rawFlags){
   return agreement
 }
 
-function _getValues(rawValues, agreement){
+function _getValues(rawValues, agreement) {
   const possibleValues = rawValues.split(',').map(v => v.trim())
+  let values = {}
+
+  if (possibleValues.length === 1) {
+    if (agreement.m && agreement.f || agreement.s && agreement.p) {
+      values = _getValuesForSameGenderOrNumber(
+        [possibleValues[0], possibleValues[0]],
+        agreement
+      )
+    } else {
+      const gender = agreement.m ? 'm' : 'f'
+      const number = agreement.s ? 's' : 'p'
+      values[`${gender}${number}`] = possibleValues[0]
+    }
+  }
+
+  if (possibleValues.length === 2) {
+    values = _getValuesForSameGenderOrNumber(possibleValues, agreement)
+  }
+
+  if (possibleValues.length === 4) {
+    values.ms = possibleValues[0]
+    values.fs = possibleValues[1]
+    values.mp = possibleValues[2]
+    values.fp = possibleValues[3]
+  }
+
+  return values
+}
+
+function _getValuesForSameGenderOrNumber(possibleValues, agreement) {
   const values = {}
-  
+  // are the 2 values genders or numbers ?
+  if (agreement.m && agreement.f) { // we have both genders, so it's gender
+    const number = agreement.s ? 's' : 'p'
+    values[`m${number}`] = possibleValues[0]
+    values[`f${number}`] = possibleValues[1]
+  } else { // number
+    const gender = agreement.m ? 'm' : 'f'
+    values[`${gender}s`] = possibleValues[0]
+    values[`${gender}p`] = possibleValues[1]
+  }
   return values
 }
 
@@ -80,5 +119,6 @@ module.exports = {
 
   // only exported for testing
   _parseParts,
-  _getAgreement
+  _getAgreement,
+  _getValues
 }
--- src/automotron/agreementUtils.test.js
+++ src/automotron/agreementUtils.test.js
@@ -1,17 +1,32 @@
 /* global describe,test,expect */
-const { parse, _parseParts, _getAgreement } = require('./agreementUtils');
+const {
+  parse,
+  _parseParts,
+  _getAgreement,
+  _getValues
+} = require('./agreementUtils');
 
 describe('Agreement utils', () => {
   describe('parse', () => {
-      test.each([
-        ['navet(ms)', {ms:'navet'}, {m:true, f:false, s:true, p:false}]
-      ])('%s', (str, expectedValues, expectedAgreement) => {
-        const {values, agreement} = parse(str)
-        //expect(values).toEqual(expectedValues)
-        expect(agreement).toEqual(expectedAgreement)
-      })
-  
+    test.each([
+      ['navet(ms)', { ms: 'navet' }, { m: true, f: false, s: true, p: false }],
+      ['[un, une](s)', { ms: 'un', fs: 'une' }, { m: true, f: true, s: true, p: false }],
+      ['[un, une](*s)', { ms: 'un', fs: 'une' }, { m: true, f: true, s: true, p: false }], // weird notation, but valid
+      ['des(p)', { mp: 'des', fp: 'des' }, { m: true, f: true, s: false, p: true }],
+      ['un mot(ms)', { ms: 'un mot' }, { m: true, f: false, s: true, p: false }],
+      ['des mots(mp)', { mp: 'des mots' }, { m: true, f: false, s: false, p: true }],
+      ['deux(*p)', { mp: 'deux', fp: 'deux' }, { m: true, f: true, s: false, p: true }],
+      ['repas(m*)', { ms: 'repas', mp: 'repas' }, { m: true, f: false, s: true, p: true }],
+      ['[mot, mots](m)', { ms: 'mot', mp: 'mots' }, { m: true, f: false, s: true, p: true }],
+      ['[beau, belle](s)', { ms: 'beau', fs: 'belle' }, { m: true, f: true, s: true, p: false }],
+      //['pomme', {}]
+    ])('%s', (str, expectedValues, expectedAgreement) => {
+      const { values, agreement } = parse(str)
+      expect(values).toEqual(expectedValues)
+      expect(agreement).toEqual(expectedAgreement)
     })
+
+  })
 
   describe('_parseParts', () => {
     test.each([
@@ -34,13 +49,13 @@ describe('Agreement utils', () => {
       ['fs', { m: false, f: true, s: true, p: false }],
       ['mp', { m: true, f: false, s: false, p: true }],
       ['fp', { m: false, f: true, s: false, p: true }],
-      ['m', {m: true, f: false, s: true, p: true}],
-      ['f', {m: false, f: true, s: true, p: true}],
-      ['s', {m: true, f: true, s: true, p: false}],
-      ['p', {m: true, f: true, s: false, p: true}],
+      ['m', { m: true, f: false, s: true, p: true }],
+      ['f', { m: false, f: true, s: true, p: true }],
+      ['s', { m: true, f: true, s: true, p: false }],
+      ['p', { m: true, f: true, s: false, p: true }],
 
-      ['m*', {m:true, f:false, s:true, p:true}],
-      ['*p', {m:true, f:true, s:false, p:true}]
+      ['m*', { m: true, f: false, s: true, p: true }],
+      ['*p', { m: true, f: true, s: false, p: true }]
     ])('%s', (rawFlags, expectedAgreement) => {
       expect(
         _getAgreement(rawFlags)
@@ -50,14 +65,16 @@ describe('Agreement utils', () => {
 
   describe('_getValues', () => {
     test.each([
-      ['mot', {m:true, f:false, s:true, p:false}, {ms:'mot'}],
-      ['mots', {m:true, f:false, s:false, p:true}, {mp:'mots'}],
-      ['mot, mots', {m: true, f: false, s: true, p: true}, {ms: 'mot', mp: 'mots'}],
-      ['beau, belle', {m: true, f: true, s: true, p: false}, {ms: 'mot', mp: 'mots'}],
-      ['beau, belle, beaux, belles', {m: true, f: true, s: true, p: true}, {ms: 'mot', mp: 'mots'}],
-      ['deux', {m:true, f:true, s:false, p:true}, {mp:'deux', fp:'deux'}]
-    ])('%s %p', () => {
-
+      ['mot', { m: true, f: false, s: true, p: false }, { ms: 'mot' }],
+      ['mots', { m: true, f: false, s: false, p: true }, { mp: 'mots' }],
+      ['mot, mots', { m: true, f: false, s: true, p: true }, { ms: 'mot', mp: 'mots' }],
+      ['beau, belle', { m: true, f: true, s: true, p: false }, { ms: 'beau', fs: 'belle' }],
+      ['beau, belle, beaux, belles', { m: true, f: true, s: true, p: true }, { ms: 'beau', fs: 'belle', mp: 'beaux', fp: 'belles' }],
+      ['deux', { m: true, f: true, s: false, p: true }, { mp: 'deux', fp: 'deux' }],
+    ])('%s %p', (str, agreement, expectedValues) => {
+      expect(
+        _getValues(str, agreement)
+      ).toEqual(expectedValues)
     })
-  }) 
+  })
 })
